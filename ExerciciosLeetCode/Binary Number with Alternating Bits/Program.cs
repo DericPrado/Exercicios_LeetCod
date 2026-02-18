59d30d4e024a8473d6c159ede9543835b23cee0b
Deric <dericprado@gmail.com>
2026-02-18 06:50:05 -0300

    Iniciando estrutura basica

---
+++ ExerciciosLeetCode/Binary Number with Alternating Bits/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Binary_Number_with_Alternating_Bits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
