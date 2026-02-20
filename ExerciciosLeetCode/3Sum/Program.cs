af2cf02f8b6660cac38c3e9e0f7158ce758821f4
Deric <dericprado@gmail.com>
2026-02-20 07:06:05 -0300

    Iniciando outro desafio

---
+++ ExerciciosLeetCode/3Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _3Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Segue o link do desafio: https://leetcode.com/problems/3sum/description/
+            */
+            int[] nums = [-1, 0, 1, 2, -1, -4];
+            var res = ThreeSum(nums);
+
+            Console.WriteLine(res);
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var res = new List<IList<int>>();
+        }
+    }
+}
