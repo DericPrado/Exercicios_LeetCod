2ca8fe2cedebcf3bf1b0af712ac93034c1c45dc9
Deric <dericprado@gmail.com>
2026-02-23 06:08:38 -0300

    Resolvendo o problema

--- ExerciciosLeetCode/3Sum/Program.cs
+++ ExerciciosLeetCode/3Sum/Program.cs
@@ -16,6 +16,43 @@
         public static IList<IList<int>> ThreeSum(int[] nums)
         {
             var res = new List<IList<int>>();
+            int soma = 0;
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    soma = nums[i] + nums[left] + nums[right];
+
+                    if(soma == 0)
+                    {
+                        res.Add([nums[i], nums[left], nums[right]]);
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+
+                    else if(soma < 0)
+                    {
+                        left++;
+                    }
+
+                    else if(soma > 0)
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return res;
         }
     }
 }
