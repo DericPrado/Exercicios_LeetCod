dbb344a26ae447e5c19d208ccb357fc4cee0086b
Deric <dericprado@gmail.com>
2026-03-04 06:36:58 -0300

    Mais um desafio

---
+++ ExerciciosLeetCode/Find Kth Bit in Nth Binary String/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Find_Kth_Bit_in_Nth_Binary_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Segue o link: https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/description/?envType=daily-question&envId=2026-03-03
+             */
+        }
+
+        public char FindKthBit(int n, int k)
+        {
+
+        }
+    }
+}
