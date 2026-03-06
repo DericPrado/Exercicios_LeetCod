3834731c0e5becdd0a353fe46fefd224e982bab9
Deric <dericprado@gmail.com>
2026-03-06 06:07:07 -0300

    Concluindo mais 1 desafio

---
+++ ExerciciosLeetCode/Check if Binary String Has at Most One Segment of Ones/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Segue o link do desafio: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/?envType=daily-question&envId=2026-03-06
+             */
+            string s = "1001";
+            Console.Write(CheckOnesSegment(s));
+        }
+
+        public static bool CheckOnesSegment(string s)
+        {
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1')
+                {
+                    if (i == 0)
+                    {
+                        count++;
+                    }
+                    else if (s[i - 1] == s[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                        break;
+                    }
+                }
+            }
+
+            if(count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
