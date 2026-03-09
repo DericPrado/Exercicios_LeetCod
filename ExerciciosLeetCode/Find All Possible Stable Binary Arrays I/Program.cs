8307c15032949daf530a513e55f70a834ba9adbb
Deric <dericprado@gmail.com>
2026-03-09 06:46:38 -0300

    Iniciando outro desafio

---
+++ ExerciciosLeetCode/Find All Possible Stable Binary Arrays I/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Find_All_Possible_Stable_Binary_Arrays_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 Segue o link do desafio: https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/description/?envType=daily-question&envId=2026-03-09
+             */
+        }
+
+        public int NumberOfStableArrays(int zero, int one, int limit)
+        {
+
+        }
+    }
+}
