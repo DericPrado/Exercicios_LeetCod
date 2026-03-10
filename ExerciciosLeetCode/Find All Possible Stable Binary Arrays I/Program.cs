dd501cce400290738d0930a31295becac1d33576
Deric <dericprado@gmail.com>
2026-03-10 06:17:03 -0300

    Concluindo o desafio

--- ExerciciosLeetCode/Find All Possible Stable Binary Arrays I/Program.cs
+++ ExerciciosLeetCode/Find All Possible Stable Binary Arrays I/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Find_All_Possible_Stable_Binary_Arrays_I
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace Find_All_Possible_Stable_Binary_Arrays_I
 {
     internal class Program
     {
@@ -11,7 +13,40 @@
 
         public int NumberOfStableArrays(int zero, int one, int limit)
         {
+            int MOD = 1000000007;
+            long[,,] dp = new long[zero + 1, one + 1, 2];
 
+            for (int i = 1; i <= Math.Min(zero, limit); i++)
+            {
+                dp[i, 0, 0] = 1;
+            }
+            for (int j = 1; j <= Math.Min(one, limit); j++)
+            {
+                dp[0, j, 1] = 1;
+            }
+
+            for (int i = 1; i <= zero; i++)
+            {
+                for (int j = 1; j <= one; j++)
+                {
+                    dp[i, j, 0] = (dp[i - 1, j, 0] + dp[i - 1, j, 1]) % MOD;
+
+                    if (i > limit)
+                    {
+                        dp[i, j, 0] = (dp[i, j, 0] - dp[i - 1 - limit, j, 1] + MOD) % MOD;
+                    }
+
+                    dp[i, j, 1] = (dp[i, j - 1, 1] + dp[i, j - 1, 0]) % MOD;
+
+                    if (j > limit)
+                    {
+                        dp[i, j, 1] = (dp[i, j, 1] - dp[i, j - 1 - limit, 0] + MOD) % MOD;
+                    }
+                }
+            }
+
+            long total = (dp[zero, one, 0] + dp[zero, one, 1]) % MOD;
+            return (int)total;
         }
     }
 }
